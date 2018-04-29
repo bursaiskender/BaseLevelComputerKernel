56bfdfcb0b9bf00b5dcfd146045937e7a5c645f5
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-04-29 21:00:23 +0300

    kernel bootloader improved, added function for waiting key to load kernel #1

--- src/bootloader/balecok.asm
+++ src/bootloader/balecok.asm
@@ -1,58 +1,95 @@
-	BITS 16
+    [BITS 16]
 
-start:
-	mov ax, 07C0h		
-	add ax, 288		
-	mov sp, 4096
+start: ; Starting process
+    
+    ; set stack space and segment
+    mov ax, 07C0h
+    add ax, 288
+    mov ss, ax
+    mov sp, 4096
+    ; set stack space and segment
+    
+    ; set data segments
+    mov ax, 07C0h 
+    mov ds, ax 
+    ; set data segments
+    
+    
+    ; introduce bootloader to user
+    call new_line
 
-	mov ax, 07C0h		
-	mov ds, ax
+    mov si, header_0
+    call print_line
 
-    call nwl ; add new line
+    mov si, header_1
+    call print_line
 
-	mov si, giristext_1
-	call prl ; print line statement
+    call new_line
 
-    mov si, giristext_2
-	call prl ; print line statement
+    mov si, press_key_msg
+    call print_line
 
-    mov si, giristext_3
-	call prl ; print line statement
+    call new_line
 
-    call nwl ; add new line
+    ; A20 gate part
+    in         al, 0x92
+    or         al, 2
+    out         0x92, al
+    
+    call key_wait ; wait any key for starting process
 
-	jmp $
+    mov si, load_kernel
+        call print_line
+    ; introduce bootloader to user
+    
+    jmp $ ; fucking infinite loop
 
 
-nwl: ; new line
-	mov ah, 0Eh
-
+new_line:
+    mov ah, 0Eh
     mov al, 0Ah
     int 10h
-
     mov al, 0Dh
     int 10h
-
+    
     ret
 
-prl: ; print line
-	mov ah, 0Eh
+print_line:
+    mov ah, 0Eh
 
 .repeat:
-	lodsb			
-	cmp al, 0
-	je .done		
-	int 10h			
-	jmp .repeat
+    lodsb
+    cmp al, 0
+    je .done
+    int 10h
+    jmp .repeat
 
 .done:
-    call nwl
+    call new_line
+    ret
+
+key_wait:
+    mov        al, 0xD2
+    out        64h, al
+    mov        al, 0x80
+    out        60h, al
+    keyup:
+        in        al, 0x60
+        and         al, 10000000b
+    jnz        keyup
+    Keydown:
+    in        al, 0x60
 
     ret
 
-	giristext_1 db 'Yerli Milli ------ Yerli Milli', 0
-	giristext_2 db 'Kernel ---  BaLeCoK --- Kernel', 0
-	giristext_3 db 'Yerli Milli ------ Yerli Milli', 0
+; Defines
 
-	times 510-($-$$) db 0	
-	dw 0xAA55		
+    header_0 db 'BaLeCoK -> Base Level Computer Kernel', 0
+    header_1 db 'Developed and Maintained by @BTaskaya', 0
+
+    press_key_msg db 'Press any key to boot kernel...', 0
+    load_kernel db 'Attempt to boot the kernel...', 0
+
+; Boot Sector
+times 510-($-$$) db 0
+dw 0xAA55
