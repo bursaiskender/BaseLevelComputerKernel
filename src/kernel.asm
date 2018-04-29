116497778b20861aa157f0cbcef68c9b3cc9b1b5
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-04-29 23:05:01 +0300

    64 bit support improved, a tiny interface created #3

--- src/kernel.asm
+++ src/kernel.asm
@@ -5,6 +5,40 @@ jmp _start
 
 %include "src/utils/intel_16.asm" ; Include common functions
 
+
+%define BLACK_F 0x0
+%define BLUE_F 0x1
+%define GREEN_F 0x2
+%define CYAN_F 0x3
+%define RED_F 0x4
+%define PINK_F 0x5
+%define ORANGE_F 0x6
+%define WHITE_F 0x7
+
+%define BLACK_B 0x0
+%define BLUE_B 0x1
+%define GREEN_B 0x2
+%define CYAN_B 0x3
+%define RED_B 0x4
+%define PINK_B 0x5
+%define ORANGE_B 0x6
+%define WHITE_B 0x7
+
+%define STYLE(f,b) ((f << 4) + b)
+
+%macro PRINT_B 3
+    mov rdi, TRAM
+    mov rbx, %1
+    mov dl, STYLE(%2, %3)
+    call print_string
+%endmacro
+
+%macro PRINT_P 3
+    mov rbx, %1
+    mov dl, STYLE(%2, %3)
+    call print_string
+%endmacro
+
 _start:
     xor ax, ax
     mov ds, ax
@@ -58,20 +92,25 @@ pm_start:
     xor eax, eax
     rep stosd
 
-    mov     dword [0x70000], 0x71000 + 7    
-    mov     dword [0x71000], 0x72000 + 7   
-    mov     dword [0x72000], 0x73000 + 7    
+    mov dword [0x70000], 0x71000 + 7    
+    mov dword [0x71000], 0x72000 + 7   
+    mov dword [0x72000], 0x73000 + 7    
 
-    mov     edi, 0x73000                    
-    mov     eax, 7
-    mov     ecx, 256                        
+    mov edi, 0x73000                    
+    mov eax, 7
+    mov ecx, 256                        
 
     make_page_entries:
         stosd
         add     edi, 4
         add     eax, 0x1000
         loop    make_page_entries
-
+    
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 100000000b
+    wrmsr
+    
     mov     eax, 0x70000    
     mov     cr3, eax        
 
@@ -85,14 +124,56 @@ pm_start:
 [BITS 64]
     
 lm_start:
+    call clear_screen
+    mov rdi, TRAM + 0x14 * 8
+    PRINT_P command_line, BLACK_F, WHITE_B
+    
     jmp $
+
+clear_screen:
+    PRINT_B header_title, WHITE_F, BLACK_B
+
+    mov rdi, TRAM + 0x14 * 8
+
+    mov rcx, 0x14 * 24
+    mov rax, 0x0720072007200720
+    rep stosq
+
+    ret
+
+print_string:
+    push rax
+
+.repeat:
+    mov al, [rbx]
+
+    cmp al, 0
+    je .done
+
+    stosb
+
+    mov al, dl
+    stosb
+
+    inc rbx
+
+    jmp .repeat
+
+.done:
+    pop rax
+
+    ret
     
 ; Defines
 
     kernel_header_0 db 'BaLeCoK -> Base Level Computer Kernel', 0
     kernel_header_1 db 'Developed and Maintained by @BTaskaya', 0
     kernel_header_2 db 'Welcome to the our Kernel Part', 0
-
+    header_title db "                                    BaLeCoK                                     ", 0
+    command_line db "root@balecok $ ", 0
+    TRAM equ 0x0B8000
+    VRAM equ 0x0A0000
+    
 GDT64:
     NULL_SELECTOR:
         dw GDT_LENGTH   
@@ -123,4 +204,4 @@ GDT64:
    GDT_LENGTH:
 
    ; Boot Sector
-   times 512-($-$$) db 0
+   times 1024-($-$$) db 0
