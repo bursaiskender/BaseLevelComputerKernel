59cb5dc81b68bd975aadede86882e08d3a5476fd
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-04-30 01:38:52 +0300

    int_str_length utility moved into utils/utils.asm #7

--- src/shell.asm
+++ src/shell.asm
@@ -132,35 +132,6 @@ shell_start:
             call print_normal
 
             jmp .start_waiting
-
-int_str_length:
-    push rbx
-    push rdx
-    push rsi
-
-    mov rax, r8
-
-    xor rsi, rsi
-
-    .loop:
-        xor rdx, rdx
-        mov rbx, 10
-        div rbx
-        add rdx, 48
-
-        inc rsi
-
-        cmp rax, 0
-        jne .loop
-
-    .exit:
-        mov rax, rsi
-
-        pop rsi
-        pop rdx
-        pop rbx
-
-        ret
         
 goto_next_line:
     push rax
---
+++ src/utils/utils.asm
@@ -0,0 +1,38 @@
+int_str_length:
+    push rbx
+    push rdx
+    push rsi
+
+    mov rax, r8
+
+    xor rsi, rsi
+        mov rbx, 10
+        div rbx
+        add rdx, 48
+
+        inc rsi
+
+        cmp rax, 0
+        jne .loop
+
+
+    .loop:
+        xor rdx, rdx
+        mov rbx, 10
+        div rbx
+        add rdx, 48
+
+        inc rsi
+
+        cmp rax, 0
+        jne .loop
+
+    .exit:
+        mov rax, rsi
+
+        pop rsi
+        pop rdx
+        pop rbx
+
+        ret
+        
