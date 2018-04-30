b2e045b0d0f9fc916d3b17267c809d35fbcf2dd5
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-04-30 18:15:32 +0300

    Unnecesary function removed

--- src/utils/keyboard.asm
+++ src/utils/keyboard.asm
@@ -4,22 +4,6 @@ key_to_ascii:
     mov al, [eax + qwerty]
     ret
 
-key_wait:
-    mov al, 0xD2
-    out 0x64, al
-
-    mov al, 0x80
-    out 0x60, al
-
-    .key_up:
-        in al, 0x60
-        and al, 10000000b
-    jnz .key_up
-
-        in al, 0x60
-
-    ret
-
 qwerty:
     db '0',0xF,'1234567890',0xF,0xF,0xF,0xF
     db 'qwertyuiop'
