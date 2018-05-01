4f7009a884c2ece4922c19f976fca237fd9d0711
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-05-01 18:34:14 +0300

    Init memory manager, #30

--- kernel/include/memory.hpp
+++ kernel/include/memory.hpp
@@ -14,5 +14,7 @@ void load_memory_map();
 bool mmap_failed();
 std::size_t mmap_entry_count();
 const mmapentry& mmap_entry(std::size_t i);
+void init_memory_manager();
+std::size_t* k_malloc(std::size_t bytes);
 
 #endif
--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -5,7 +5,7 @@
 extern "C" {
 void  __attribute__ ((section ("main_section"))) kernel_main(){
     load_memory_map();
-    init_shell();
+    init_memory_manager();
     install_timer();
 
     return;
