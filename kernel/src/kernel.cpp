c800af1b5284b38025d7462611de01a750ec9a51
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-05-01 11:12:38 +0300

    Created own source/module's for kernel files #13 #14

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,40 +1,15 @@
 #include <cstddef>
 
 #include "types.hpp"
-#include "addresses.hpp"
 #include "keyboard.hpp"
-
-typedef unsigned int uint8_t __attribute__((__mode__(__QI__)));
-typedef unsigned int uint16_t __attribute__ ((__mode__ (__HI__)));
-
-void k_print(char key);
-void k_print(const char* string);
-void k_print_line(const char* string);
-
-uint8_t in_byte(uint16_t _port){
-    uint8_t rv;
-    __asm__ __volatile__ ("in %0, %1" : "=a" (rv) : "dN" (_port));
-    return rv;
-}
-
-void out_byte (uint16_t _port, uint8_t _data){
-    __asm__ __volatile__ ("out %0, %1" : : "dN" (_port), "a" (_data));
-}
-
-template<uint8_t IRQ>
-void register_irq_handler(void (*handler)()){
-    asm ("mov r8, %0; mov r9, %1; call %2"
-        :
-        : "I" (IRQ), "r" (handler), "i" (asm_register_irq_handler)
-        : "r8", "r9"
-        );
-}
+#include "kernel_utils.hpp"
+#include "console.hpp"
 
 void keyboard_handler();
 
 extern "C" {
 void  __attribute__ ((section ("main_section"))) kernel_main(){
-    k_print("root@balecok # ");
+    k_print("root@blaecok # ");
 
     register_irq_handler<1>(keyboard_handler);
 
@@ -49,75 +24,16 @@ void keyboard_handler(){
     uint8_t key = in_byte(0x60);
 
     if(key & 0x80){
-        // balecok-todo: handle_shift
     } else {
         if(key == 0x1C){
-            // balecok-todo: enter
         } else if(key == 0x0E){
-            // balecok-todo: backspace
         } else {
-           auto qwerty_key = key_to_ascii(key);
+           auto qwertz_key = key_to_ascii(key);
 
-           current_input[current_input_length++] = qwerty_key;
-           k_print(qwerty_key);
+           if(qwertz_key > 0){
+               current_input[current_input_length++] = qwertz_key;
+               k_print(qwertz_key);
+           }
         }
     }
-}
-
-
-long current_line = 0;
-long current_column = 0;
-
-enum vga_color {
-    BLACK = 0,
-    BLUE = 1,
-    GREEN = 2,
-    CYAN = 3,
-    RED = 4,
-    MAGENTA = 5,
-    BROWN = 6,
-    LIGHT_GREY = 7,
-    DARK_GREY = 8,
-    LIGHT_BLUE = 9,
-    LIGHT_GREEN = 10,
-    LIGHT_CYAN = 11,
-    LIGHT_RED = 12,
-    LIGHT_MAGENTA = 13,
-    LIGHT_BROWN = 14,
-    WHITE = 15,
-};
-
-uint8_t make_color(vga_color fg, vga_color bg){
-    return fg | bg << 4;
-}
-
-uint16_t make_vga_entry(char c, uint8_t color){
-    uint16_t c16 = c;
-    uint16_t color16 = color;
-    return c16 | color16 << 8;
-}
-
-void k_print_line(const char* string){
-    k_print(string);
-
-    current_column = 0;
-    ++current_line;
-}
-
-void k_print(char key){
-    uint16_t* vga_buffer = (uint16_t*) 0x0B8000;
-
-    vga_buffer[current_line * 80 + current_column] = make_vga_entry(key, make_color(WHITE, BLACK));
-
-    ++current_column;
-
-    return;
-}
-
-void k_print(const char* string){
-    for(int i = 0; string[i] != 0; ++i){
-        k_print(string[i]);
-    }
-
-    return;
 }
