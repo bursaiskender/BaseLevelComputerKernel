fd798c1cfc2147c518656d8aa349498f87c81b73
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-05-01 15:59:03 +0300

    move str_contains str_copy str_until str_from to utils.cpp

--- kernel/include/utils.hpp
+++ kernel/include/utils.hpp
@@ -4,5 +4,8 @@
 #include <cstddef>
 
 std::size_t parse(const char* str);
-
+bool str_contains(const char* a, char c);
+void str_copy(const char* a, char* b);
+const char* str_until(char* a, char c);
+const char* str_from(char* a, char c);
 #endif
