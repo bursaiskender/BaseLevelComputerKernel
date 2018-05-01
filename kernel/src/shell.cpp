6326141efff99e784ebad0bb5e9d085d5fab7ef8
BTaskaya <batuhanosmantaskaya@gmail.com>
2018-05-01 15:36:02 +0300

    sleep_ms method implemented, sleep command created

--- kernel/src/shell.cpp
+++ kernel/src/shell.cpp
@@ -15,17 +15,21 @@ void help_command();
 void uptime_command();
 void clear_command();
 void date_command();
+void sleep_command();
 
 struct command_definition {
     const char* name;
     void (*function)();
 };
 
-std::array<command_definition, 5> commands = { { { "reboot", reboot_command },
+std::array<command_definition, 6> commands = { { 
+    { "reboot", reboot_command },
     { "help", help_command },
     { "uptime", uptime_command },
     { "clear", clear_command },
-    { "date", date_command } } };
+    { "date", date_command } ,
+    { "sleep", sleep_command },
+    } };
 
 std::size_t current_input_length = 0;
 char current_input[50];
@@ -98,6 +102,9 @@ void uptime_command()
     k_print("Uptime: ");
     k_print(timer_seconds());
     k_print_line("s");
+}
+void sleep_command(){
+    sleep_ms(5000);
 }
 
 void exec_command()
